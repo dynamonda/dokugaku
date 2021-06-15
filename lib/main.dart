7a20696deb1ef3d29cd3200e643342e84a12049d
dynamonda <v7gj9kk@gmail.com>
2021-06-16 06:41:19 +0900

    見た目のレイアウトを作成

--- lib/main.dart
+++ lib/main.dart
@@ -5,24 +5,14 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Dokugaku'),
     );
   }
 }
@@ -51,19 +41,63 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            ListTile(
+              title: Text('項目1'),
+              onTap: (){
+
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            ListTile(
+              title: Text('項目2'),
+              onTap: (){
+
+              },
+            )
           ],
         ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: Container(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    'メモ',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ),
+              ),
+              ListView(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                children: <Widget>[
+                  ListTile(
+                    leading: Icon(Icons.folder),
+                    title: Text('フォルダ1')
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.note),
+                    title: Text('メモ1')
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.text_snippet),
+                    title: Text('メモ2')
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
