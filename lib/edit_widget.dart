447ed745fb2165699abb4ae6df8171e6990c0320
dynamonda <v7gj9kk@gmail.com>
2021-06-24 08:11:07 +0900

    タイトルの表示位置を設定

--- lib/edit_widget.dart
+++ lib/edit_widget.dart
@@ -16,8 +16,24 @@ class EditWidget extends StatelessWidget {
             onPressed: () {
               Navigator.pop(context);
             }),
-        title: new Text("$_title($_uuid)"),
+        title: new Text(this._uuid),
       ),
+      body: SafeArea(
+          child: Column(
+        children: [
+          Container(
+            color: Colors.amber,
+            width: double.infinity,
+            padding: EdgeInsets.all(10.0),
+            child: Text(_title,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                )),
+          ),
+          Container(color: Colors.teal),
+        ],
+      )),
     );
   }
 }
